85e698e416be2c67b498988d5e599e49ebb4f0e2
Casey Greene <csgreene@princeton.edu>
2011-05-20 11:03:57 -0400

    Format source files, update go.py to use an annotation class to keep track of references more easily

--- dat.py
+++ dat.py
@@ -5,49 +5,49 @@ import array
 import struct
 
 class dat:
-    def __init__(self,filename):
-        self.gene_list=[]
-        self.gene_table={}
+    def __init__(self, filename):
+        self.gene_list = []
+        self.gene_table = {}
         self.open_file(filename)
-        self.gene_index={}
+        self.gene_index = {}
         for i in range(len(self.gene_list)):
-            self.gene_index[self.gene_list[i]]=i
+            self.gene_index[self.gene_list[i]] = i
 
-    def open_file(self,filename):
-        dab_file = open(filename, 'rb' )
+    def open_file(self, filename):
+        dab_file = open(filename, 'rb')
 
         #get number of genes
         a = array.array('I')
-        a.fromfile(dab_file,1)
+        a.fromfile(dab_file, 1)
         #print "#nodes="+str(a[0])
-        size=a[0]
+        size = a[0]
 
         #get gene names
-        start=4
-        end=4
-        count=0
+        start = 4
+        end = 4
+        count = 0
         while count < a[0]:
             dab_file.seek(end)
-            if( dab_file.read(2)=='\0\0' ):
+            if(dab_file.read(2) == '\0\0'):
                 dab_file.seek(start)
 
-                gene = dab_file.read(end-start+1)
+                gene = dab_file.read(end - start + 1)
                 gene = gene.strip()
-                gene = gene.replace('\x00','')
+                gene = gene.replace('\x00', '')
 
                 self.gene_list.append(gene)
-                self.gene_table[gene]=count
+                self.gene_table[gene] = count
 
-                start=end+2
-                count+=1
-                end+=1
-            end+=1
+                start = end + 2
+                count += 1
+                end += 1
+            end += 1
 
         #get half matrix values
-        total=(size*(size-1))/2
+        total = (size * (size - 1)) / 2
         dab_file.seek(start)
         self.dat = array.array('f')
-        self.dat.fromfile(dab_file,total)
+        self.dat.fromfile(dab_file, total)
 
         assert len(self.dat) == total
         #print "#edges="+str(total)
@@ -68,13 +68,13 @@ class dat:
         #except ValueError:
         #       return None
 
-        g1 = min( gene1, gene2 )
-        g2 = max( gene1, gene2 )
+        g1 = min(gene1, gene2)
+        g2 = max(gene1, gene2)
 
-        start=self.arith_sum( (len(self.gene_list))-g1, (len(self.gene_list)-1) ) #index of first id
-        start += (g2-g1)-1 #index of second id
+        start = self.arith_sum((len(self.gene_list)) - g1, (len(self.gene_list) - 1)) #index of first id
+        start += (g2 - g1) - 1 #index of second id
         try:
-            v= self.dat[int(start)]
+            v = self.dat[int(start)]
         except IndexError:
             print 'Error: ', start, gene1, gene2
             exit()
@@ -98,33 +98,33 @@ class dat:
         except KeyError:
             return None
 
-    def arith_sum(self, x, y ):
-        return .5 * (y-x+1) * (x+y)
+    def arith_sum(self, x, y):
+        return .5 * (y - x + 1) * (x + y)
 
     def print_table(self):
         cols = ['']
         cols.extend(self.gene_list)
         print "\t".join(cols)
 
-        for i in range(0,self.get_size()):
-            line=[]
+        for i in range(0, self.get_size()):
+            line = []
             line.append(self.gene_list[i])
-            for j in range(0,i):
-                v = self.get_value(i,j)
+            for j in range(0, i):
+                v = self.get_value(i, j)
                 line.append(str(v))
             line.append("0")
-            for j in range(i+1,self.get_size()):
-                v = self.get_value(i,j)
+            for j in range(i + 1, self.get_size()):
+                v = self.get_value(i, j)
                 line.append(str(v))
 
             print "\t".join(line)
 
 
     def convert_mfinder(self):
-        for i in range(0,self.get_size()):
-            for j in range(i+1,self.get_size()):
-                v = self.get_value(i,j)
-                if( v < .01 ):
+        for i in range(0, self.get_size()):
+            for j in range(i + 1, self.get_size()):
+                v = self.get_value(i, j)
+                if(v < .01):
                     print str(i) + " " + str(j)# + " 1"
 
 
@@ -137,3 +137,4 @@ class dat:
 #id1 = d.gene_list.index('MGI:3617846')
 #id2 = d.gene_list.index('MGI:2685617')
 #print d.get_value(id1,id2)
+
--- idmap.py
+++ idmap.py
@@ -5,7 +5,7 @@ class idmap:
     """
     Pass the filename of the key_value pair file.
     """
-    def __init__(self, filename, list = None):
+    def __init__(self, filename, list=None):
         self.key_val = {}
         idfile = list
         if filename is not None:
@@ -27,7 +27,7 @@ class idmap:
     """
     Returns None if no file was loaded or if the key does not exist.
     """
-    def get(self, id = None):
+    def get(self, id=None):
         upper_id = None
         if id is not None:
             upper_id = id.upper()
@@ -38,3 +38,4 @@ class idmap:
                 return self.key_val[upper_id]
             except KeyError:
                 return None
+
