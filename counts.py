bfe4d81f43951744f5de41ec47dd61248765ed6b
Daniel Himmelstein <dhimmel@users.noreply.github.com>
2011-07-27 18:58:06 -0400

    average proportions working

--- counts.py
+++ counts.py
@@ -7,6 +7,7 @@ class Counts:
         self.num_datasets = 0
         self.tot_posits = 0
         self.tot_negats = 0
+        #self.ptables = []
     
     def read(self, filename):
         self.filename = filename
@@ -28,13 +29,12 @@ class Counts:
             dataset_name = f.readline().strip()
         f.close()
         
-    # Not currently working when printing an object
     def __repr__(self):
         return "Counts: " + self.name
     
     def __add__(self, other):
         sum_count = Counts()
-        #sum_count.name = self.name + " + " + other.name
+        # sum_count.name = self.name + " + " + other.name
         sum_count.num_datasets = self.num_datasets
         sum_count.tot_negats = self.tot_negats + other.tot_negats
         sum_count.tot_posits = self.tot_posits + other.tot_posits
@@ -54,7 +54,6 @@ class Counts:
     def __rmul__(self, other):
         return(self.__mul__(other))
 
-    
     def write_counts(self, filename):
         file = open(filename, 'w')
         file.write(self.name + "\t" + str(self.num_datasets) + "\n")
@@ -64,12 +63,36 @@ class Counts:
             file.write('\t'.join([str(int(x)) for x in self.tables[i].neg_bins]) + '\n')
             file.write('\t'.join([str(int(x)) for x in self.tables[i].pos_bins]) + '\n')
         file.close()
+    
+    def to_props(self):
+        for x in self.tables:
+            x.to_props()
+    
+    @staticmethod
+    def ave_props(counts_list):
+        ave_counts = Counts()
+        ave_counts.num_datasets = counts_list[0].num_datasets
+        ave_counts.tot_negats = 1
+        ave_counts.tot_posits = 1
+        for i in xrange(ave_counts.num_datasets):
+            ave_counts.tables.append(ConTable.ave_props([x.tables[i] for x in counts_list]))
+        return(ave_counts)
+
+    def counts_from_props(self, mult = 100000):
+        self.tot_negats = mult
+        self.tot_posits = mult
+        for c_table in self.tables:
+            c_table.neg_bins = [int(x * mult) for x in c_table.neg_props]
+            c_table.pos_bins = [int(x * mult) for x in c_table.pos_props] 
+
 
 
 class ConTable:
     
     def __init__(self, name="", neg_bins=None, pos_bins=None):
         self.name = name
+        self.neg_props = []
+        self.pos_props = []
         if neg_bins:
             self.neg_bins = neg_bins[:]
         else:
@@ -92,6 +115,33 @@ class ConTable:
         else:
             raise TypeError
     
+    def to_props(self):
+        tot_negats = sum(self.neg_bins)
+        tot_posits = sum(self.pos_bins)
+        if tot_negats == 0:
+            bins = len(self.neg_bins)
+            self.neg_props = [1.0 / bins] * bins
+        else:
+            self.neg_props = [float(x) / tot_negats for x in self.neg_bins]
+        if tot_posits == 0:
+            bins = len(self.neg_bins)
+            self.pos_props = [1.0 / bins] * bins
+        else:
+            self.pos_props = [float(x) / tot_posits for x in self.pos_bins]
+    
+    @staticmethod
+    def ave_props(con_table_list):
+        num_of_con_tables = len(con_table_list)
+        num_of_bins = len(con_table_list[0].neg_props)
+        c = ConTable()
+        c.name = con_table_list[0].name
+        for i in range(num_of_bins):
+            c.neg_props.append(sum([x.neg_props[i] for x in con_table_list]) /
+                               num_of_con_tables)
+            c.pos_props.append(sum([x.pos_props[i] for x in con_table_list]) /
+                               num_of_con_tables)
+        return(c)
+
     def __radd__(self, other):
         return(self.__add__(other))
         
