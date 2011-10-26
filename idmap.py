5db0c523c1c6730ad351f7986f92f6b13c212962
Casey Greene <cgreene@users.noreply.github.com>
2011-10-26 10:05:56 -0400

    add main to flib/idmap

--- idmap.py
+++ idmap.py
@@ -1,4 +1,8 @@
 import sys
+import logging
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
 
 class idmap:
     key_val = None
@@ -32,10 +36,37 @@ class idmap:
         if id is not None:
             upper_id = id.upper()
         if self.key_val is None:
+            logger.info('idmap::get called with no mapping file loaded')
             return None
         else:
             try:
                 return self.key_val[upper_id]
             except KeyError:
+                logger.warning('No match for %s', id)
                 return None
+
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+    usage = "usage: %prog [options]"
+    parser = OptionParser(usage, version="%prog dev-unreleased")
+    parser.add_option("-i", "--input-file", dest="input", help="input file", metavar="FILE")
+    parser.add_option("-m", "--mappings-file", dest="mapping", help="mappings file", metavar="FILE")
+    (options, args) = parser.parse_args()
+
+    if options.input is None:
+        sys.stderr.write("--input-file is required.\n")
+        sys.exit()
+    if options.mapping is None:
+        sys.stderr.write("--mappings-file is required.\n")
+        sys.exit()
+    
+    id_name = idmap(options.mapping)
+    
+    for line in open(options.input):
+        vals = id_name.get(line.strip())
+        if vals is None:
+            continue
+        for val in vals:
+            print(val)
 
